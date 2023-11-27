e69fde9d1cc3e410105d396c076c23faf41a5460
SSC202 <e22750706642022@163.com>
2023-11-27 11:39:14 +0800

    存在异色遮挡缺陷

--- Python_Test/project_2.py
+++ Python_Test/project_2.py
@@ -48,12 +48,12 @@ lutRaisen = np.array([int(102+0.6*i) for i in range(256)]).astype("uint8")
 # 调节饱和度
 lutSRaisen = np.dstack((lutEqual, lutRaisen, lutEqual))  # Saturation raisen
 # 2. 掩膜阈值定义
-lower_green = np.array([30, 156, 58])
-upper_green = np.array([66, 223, 232])
+lower_purple = np.array([137, 143, 0])
+upper_purple = np.array([179, 203, 255])
 lower_red = np.array([117, 143, 0])
 upper_red = np.array([133, 255, 255])
 # 3. 结构元素定义
-kernel = np.ones((9, 9), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 
 #############################
 # 运行段
@@ -89,56 +89,67 @@ try:
         """
             2. 掩膜创建
         """
-        green_mask = cv2.inRange(blendSRaisen, lower_green, upper_green)
-        green_img = cv2.bitwise_and(color_image, color_image, mask=green_mask)
-        # cv2.imshow('result',green_img)
+        purple_mask = cv2.inRange(blendSRaisen, lower_purple, upper_purple)
+        purple_img = cv2.bitwise_and(
+            color_image, color_image, mask=purple_mask)
+        # cv2.imshow('result',purple_img)
         red_mask = cv2.inRange(blendSRaisen, lower_red, upper_red)
         red_img = cv2.bitwise_and(color_image, color_image, mask=red_mask)
         # cv2.imshow('result',red_img)
         """
             3. 滤波
         """
-        # green_img = cv2.GaussianBlur(green_img, (3, 3), 0)
+        # purple_img = cv2.GaussianBlur(purple_img, (3, 3), 0)
         # red_img = cv2.GaussianBlur(red_img, (3, 3), 0)
-        green_img = cv2.medianBlur(green_img, 5)
+        purple_img = cv2.medianBlur(purple_img, 5)
         red_img = cv2.medianBlur(red_img, 5)
         """
             4. 二值化
         """
-        green_gray = cv2.cvtColor(green_img, cv2.COLOR_RGB2GRAY)
+        purple_gray = cv2.cvtColor(purple_img, cv2.COLOR_RGB2GRAY)
         red_gray = cv2.cvtColor(red_img, cv2.COLOR_BGR2GRAY)
-        # green_gray = cv2.GaussianBlur(green_gray, (3, 3), 0)
+        # purple_gray = cv2.GaussianBlur(purple_gray, (3, 3), 0)
         # red_gray = cv2.GaussianBlur(red_gray, (3, 3), 0)
-        # cv2.imshow('result',green_gray)
-        # green_thre = cv2.adaptiveThreshold(green_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,-10)
+        # cv2.imshow('result',purple_gray)
+        # purple_thre = cv2.adaptiveThreshold(purple_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,-10)
         # red_thre = cv2.adaptiveThreshold(red_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,-10)
-        res, green_thre = cv2.threshold(
-            green_gray, 0, 255, cv2.THRESH_BINARY)
+        res, purple_thre = cv2.threshold(
+            purple_gray, 0, 255, cv2.THRESH_BINARY)
         res, red_thre = cv2.threshold(
             red_gray, 0, 255, cv2.THRESH_BINARY)
-        # cv2.imshow('result', green_thre)
+        # cv2.imshow('result', purple_thre)
         # cv2.imshow('result', red_thre)
 
-        green_thre = cv2.morphologyEx(green_thre, cv2.MORPH_OPEN, kernel)
+        purple_thre = cv2.morphologyEx(purple_thre, cv2.MORPH_OPEN, kernel)
         red_thre = cv2.morphologyEx(red_thre, cv2.MORPH_OPEN, kernel)
-        green_thre = cv2.morphologyEx(green_thre, cv2.MORPH_CLOSE, kernel)
+        purple_thre = cv2.morphologyEx(purple_thre, cv2.MORPH_CLOSE, kernel)
         red_thre = cv2.morphologyEx(red_thre, cv2.MORPH_CLOSE, kernel)
-        # cv2.imshow('result', red_thre)
+        cv2.imshow('result_thre', red_thre)
         """
-            5. Canny 提取边缘
+            5. 霍夫圆变换
         """
-        red_edge = cv2.Canny(red_thre, 0, 255)
+        red_circles = cv2.HoughCircles(red_thre, cv2.HOUGH_GRADIENT_ALT,
+                                       1.5, 10, param1=30, param2=0.50, minRadius=10, maxRadius=200)
+        purple_circles = cv2.HoughCircles(purple_thre, cv2.HOUGH_GRADIENT_ALT,
+                                       1.5, 10, param1=30, param2=0.50, minRadius=10, maxRadius=200)
 
-        circles = cv2.HoughCircles(red_thre, cv2.HOUGH_GRADIENT_ALT,
-                                   1.5, 10, param1=50, param2=0.83, minRadius=0, maxRadius=0)
-        if circles is not None:
-            circles = np.uint16(np.around(circles))
-            for cir_ in circles:
+        if red_circles is not None:
+            red_circles = np.uint16(np.around(red_circles))
+            print(red_circles.shape)
+            for cir_ in red_circles:
                 cir = cir_[0]
                 print('circle.shape:', cir.shape, 'circle:', cir)
                 cv2.circle(
-                    color_image, (cir[0], cir[1]), cir[2], (0, 255, 0), 2)
-                cv2.circle(color_image, (cir[0], cir[1]), 2, (0, 0, 255), 3)
+                    color_image, (cir[0], cir[1]), cir[2], (0, 255, 0), 1)
+                cv2.circle(color_image, (cir[0], cir[1]), 2, (0, 0, 255), 1)
+
+        if purple_circles is not None:
+            purple_circles = np.uint16(np.around(purple_circles))
+            for cir_ in purple_circles:
+                cir = cir_[0]
+                cv2.circle(
+                    color_image, (cir[0], cir[1]), cir[2], (0, 255, 255), 1)
+                cv2.circle(color_image, (cir[0], cir[1]), 2, (255, 0, 255), 1)
 
         cv2.imshow('result', color_image)
 
